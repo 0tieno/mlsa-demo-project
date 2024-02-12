c11b8444919538428b7d551337016c5ad5f1643d
Otieno <ronvi035@gmail.com>
2024-02-12 08:00:27 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Header from './Header.jsx'
+import Card from './card.jsx'  
+import profile1 from './assets/ronney.jpg'
+import profile2 from './assets/Khush Soni.jpg'
+import Footer from './Footer.jsx'
+import CountDown from './countDown.jsx'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <CountDown/>
+      
+      <div className="card-container">
+        <Card
+         imageSrc={profile1}
+         title={"Ronney Otieno"}
+          description={"Alpha MLSA"}
+        />
+        <Card 
+        imageSrc={profile2}
+        title={"Khush Soni"}
+        description={"Beta MLSA"}
+        />
+      </div>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+function Footer(){
+
+    return(
+        <footer className="footer">
+            <a href="#">
+            <p>&copy; {new Date().getFullYear()} happycoding! Be a force for good! &copy;</p>
+            </a>
+        </footer>
+    );
+}
+
+export default Footer
